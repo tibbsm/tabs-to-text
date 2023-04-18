37c73a8afc8c4ad0b2247697b013a65b746fea9e
tibbsm <tibbs.marc@gmail.com>
2023-04-18 13:05:28 +0900

    fixup

--- public/background.js
+++ public/background.js
@@ -5,19 +5,12 @@ chrome.browserAction.onClicked.addListener(function () {
       tabData += "- " + tab.title + " | " + tab.url + "\n";
     });
     copyToTheClipboard(tabData);
-    // navigator.clipboard.writeText(tabData).then(() => {
-    //   console.log(tabData);
-    //   alert("Clicked!");
-    // });
   });
 });
 
 async function copyToTheClipboard(textToCopy) {
   const el = document.createElement("textarea");
   el.value = textToCopy;
-  el.setAttribute("readonly", "");
-  el.style.position = "absolute";
-  el.style.left = "-9999px";
   document.body.appendChild(el);
   el.select();
   document.execCommand("copy");
