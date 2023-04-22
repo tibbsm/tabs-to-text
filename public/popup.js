fb55da5f89abbb40c96ab8360f74c0a73977e8a9
tibbsm <tibbs.marc@gmail.com>
2023-04-22 13:57:32 +0900

    use toast msg to show success

--- public/popup.js
+++ public/popup.js
@@ -1,3 +1,13 @@
+function showToast(message, duration) {
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerText = message;
+  document.body.appendChild(toast);
+  setTimeout(() => {
+    toast.remove();
+  }, duration);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const copyButton = document.getElementById("copyButton");
   copyButton.addEventListener("click", async () => {
@@ -10,5 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     await navigator.clipboard.writeText(tabInfo);
     // alert("Tab info copied to clipboard!");
+    showToast("Copied", 1000);
   });
 });
