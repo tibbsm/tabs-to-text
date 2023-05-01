901cf58714b24041a3b594692e89bf8d5e523a00
tibbsm <tibbs.marc@gmail.com>
2023-05-01 21:14:29 +0900

    fixes and refactor

--- public/popup.js
+++ public/popup.js
@@ -1,10 +1,9 @@
 const showToast = (message, duration) => {
   const toast = document.createElement("div");
+  const removeToast = () => toast.remove();
   toast.innerText = message;
   document.body.appendChild(toast);
-  setTimeout(() => {
-    toast.remove();
-  }, duration);
+  setTimeout(removeToast, duration);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,12 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (copyButton != null && chrome.tabs != null) {
     copyButton.addEventListener("click", async () => {
       const tabs = await chrome.tabs.query({});
+      console.log(tabs);
       const tabInfo = tabs
-        .filter((tab) => tab != null)
-        .map((tab) => {
-          tabInfo += "- " + tab.title + " | " + tab.url;
-        });
-      await navigator.clipboard.writeText(tabInfo.join("\n"));
+        .filter(({ title, url }) => title != null && url != null)
+        .map((tab) => "- " + tab.title + " | " + tab.url)
+        .join("\n");
+      await navigator.clipboard.writeText(tabInfo);
       showToast("Copied!", 1000);
     });
   }
