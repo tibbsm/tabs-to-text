39e497c9b81ad6a322390091f790c4d48386a5a0
tibbsm <tibbs.marc@gmail.com>
2023-05-11 13:03:44 +0900

    refactor

--- public/popup.js
+++ public/popup.js
@@ -6,18 +6,26 @@ const showToast = (message, duration) => {
   setTimeout(removeToast, duration);
 };
 
+const copyButtonClickListener = async () => {
+  if (chrome.tabs != null) {
+    const tabs = await chrome.tabs.query({});
+    const tabInfo = tabs
+      .filter(({ title, url }) => title != null && url != null)
+      .map(({ title, url }) => "- " + title + " | " + url)
+      .join("\n");
+    await navigator.clipboard.writeText(tabInfo);
+    showToast("Copied!", 1000);
+  } else {
+    console.error("Could not access chrome.tabs");
+  }
+};
+
 const setupCopyButton = () => {
   const copyButton = document.getElementById("copyButton");
-  if (copyButton != null && chrome.tabs != null) {
-    copyButton.addEventListener("click", async () => {
-      const tabs = await chrome.tabs.query({});
-      const tabInfo = tabs
-        .filter(({ title, url }) => title != null && url != null)
-        .map(({ title, url }) => "- " + title + " | " + url)
-        .join("\n");
-      await navigator.clipboard.writeText(tabInfo);
-      showToast("Copied!", 1000);
-    });
+  if (copyButton != null) {
+    copyButton.addEventListener("click", copyButtonClickListener);
+  } else {
+    console.error("Could find the copy button element");
   }
 };
 
