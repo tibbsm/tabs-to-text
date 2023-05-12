ff478b446f325cd2299193352de48966c5fadc1c
tibbsm <tibbs.marc@gmail.com>
2023-05-12 16:44:54 +0900

    refactor

--- public/popup.js
+++ public/popup.js
@@ -10,7 +10,8 @@ const copyButtonClickListener = async () => {
   if (chrome.tabs != null) {
     const tabs = await chrome.tabs.query({});
     const tabInfo = tabs
-      .filter(({ title, url }) => title != null && url != null)
+      // NOTE: some tabs may be skipped if the url can't be fetched
+      .filter(({ url }) => url != null)
       .map(({ title, url }) => "- " + title + " | " + url)
       .join("\n");
     await navigator.clipboard.writeText(tabInfo);
